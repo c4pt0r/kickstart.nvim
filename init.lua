4611f7c731279f7f7ea6ac0b94c5f181383cc4c3
dongxu <i@huangdx.net>
2024-06-18 18:54:33 -0700

    update

--- init.lua
+++ init.lua
@@ -573,7 +573,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -942,8 +942,8 @@ require('copilot').setup {
     },
   },
   filetypes = {
-    yaml = false,
-    markdown = false,
+    yaml = true,
+    markdown = true,
     help = false,
     gitcommit = false,
     gitrebase = false,
@@ -959,3 +959,4 @@ require('copilot').setup {
 -- https://github.com/neovim/nvim-lspconfig
 -- npm i -g pyright
 require('lspconfig').pyright.setup {}
+require('lspconfig').gopls.setup {}
