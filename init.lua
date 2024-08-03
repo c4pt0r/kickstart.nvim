a2ff95ece7f497e4d084df6d986dbafde5f343a2
dongxu <i@huangdx.net>
2024-08-02 20:47:50 -0700

    update

--- init.lua
+++ init.lua
@@ -575,7 +575,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -960,3 +960,4 @@ require('copilot').setup {
 -- npm i -g pyright
 require('lspconfig').pyright.setup {}
 require('lspconfig').gopls.setup {}
+require('lspconfig').rust_analyzer.setup {}
